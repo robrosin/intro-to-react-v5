26795660fc00d2109d756e22e8807d652dd83ad6
Rob Rosin <robert.rosin@assurant.com>
2021-03-16 12:24:01 -0400

    Add App and Pet components to App.js

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Winifred"),
-    React.createElement("h2", {}, "dog"),
-    React.createElement("h2", {}, "Great Dane"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix",
+    }),
   ]);
 };
 
