3b9132ca016bf3e4613f9aeb11655aa5eaa13e1c
Rob Rosin <robert.rosin@assurant.com>
2021-03-18 11:56:13 -0400

    Create Search Component

---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchParams = () => {
+    const location = "Seattle, WA";
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    location<input 
+                    id="location" 
+                    value={location} 
+                    placeholder="location" />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
