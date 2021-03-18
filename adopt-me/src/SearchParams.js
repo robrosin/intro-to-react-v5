73f19e07beb69acf2c88d1fd77fa87e821953353
Rob Rosin <robert.rosin@assurant.com>
2021-03-18 12:01:49 -0400

    Add UseState to handle search state

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-    const location = "Seattle, WA";
+    const [location, setLocation]  = useState("Seattle, WA");
 
     return (
         <div className="search-params">
+        <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
                     location<input 
                     id="location" 
                     value={location} 
-                    placeholder="location" />
+                    placeholder="Location" 
+                    onChange={ event => setLocation(event.target.value)} />
                 </label>
                 <button>Submit</button>
             </form>
