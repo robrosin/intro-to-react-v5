7d2a57e20210be5ce700956f93270163d8c41022
Rob Rosin <robert.rosin@assurant.com>
2021-03-18 16:46:39 -0400

    Add API mocking and update async call

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -8,6 +8,17 @@ const SearchParams = () => {
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+    const [pets, setPets] = useState([]);
+
+    async function requestPets() {
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal
+        })
+
+        setPets(animals || []);
+    }
 
     useEffect(() => {
         setBreeds([]);
@@ -22,7 +33,10 @@ const SearchParams = () => {
 return (
     <div className="search-params">
         <h1>{location}</h1>
-            <form>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                requestPets("");
+            }}>
                 <label htmlFor="location">
                     location<input 
                     id="location" 
