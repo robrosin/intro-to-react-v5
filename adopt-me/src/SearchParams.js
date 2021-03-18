f6229b2acac061156fe6f287b0bc6bd7259c04b0
Rob Rosin <robert.rosin@assurant.com>
2021-03-18 13:51:14 -0400

    Add Breed dropdown

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
     const [location, setLocation]  = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("Dog");
+    const [breed, setBreed] = useState("");
+    const [breeds, setBreeds] = useState([]);
 
     return (
         <div className="search-params">
@@ -13,6 +17,30 @@ const SearchParams = () => {
                     value={location} 
                     placeholder="Location" 
                     onChange={ event => setLocation(event.target.value)} />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select id="animal"
+                            value={animal}
+                            onChange={e => setAnimal(e.target.value)}
+                            onBlur={e => setAnimal(e.target.value)}>
+                                <option>All</option>
+                            {ANIMALS.map(animal => (
+                            <option key={animal} value={animal}>{animal}</option>))}
+                            </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select id="breed"
+                            value={breed}
+                            onChange={e => setBreed(e.target.value)}
+                            onBlur={e => setBreed(e.target.value)}
+                            disabled={breeds, length === 0}
+                            >
+                                <option>All</option>
+                            {breeds.map(breedString => (
+                            <option key={breed} value={breedString}>{breedString}</option>))}
+                            </select>
                 </label>
                 <button>Submit</button>
             </form>
