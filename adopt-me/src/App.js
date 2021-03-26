bd2b5cf656c97fe8efc6f640f77540f33a1ddd58
Rob Rosin <robert.rosin@assurant.com>
2021-03-26 15:21:36 -0400

    Add Details.js and Router components

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-
   return (
     <React.StrictMode>
-    <div>
-      <h1>Adopt Me!</h1>
-        <SearchParams></SearchParams>
-    </div>
+      <div>
+        <h1>Adopt Me!</h1>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
     </React.StrictMode>
   );
 };
